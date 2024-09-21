b2c17f0c18aca96c57d002d2b7f641e31bdce24b
Miguel-prog-ui <mmijares2033@gmail.com>
2024-09-21 18:41:15 -0400

    g

--- content.php
+++ content.php
@@ -59,16 +59,35 @@ echo "<!DOCTYPE html>
         #content {
             margin-top: 20px;
         }
+        .button {
+            display: inline-block;
+            padding: 10px 20px;
+            font-size: 16px;
+            cursor: pointer;
+            text-align: center;
+            text-decoration: none;
+            outline: none;
+            color: #fff;
+            background-color: #333;
+            border: none;
+            border-radius: 15px;
+            box-shadow: 0 9px #999;
+        }
+        .button:hover {background-color: #555}
+        .button:active {
+            background-color: #555;
+            box-shadow: 0 5px #666;
+            transform: translateY(4px);
+        }
     </style>
 </head>
 <body>
-    <nav>
-        <a href='index.html'>Volver al Menú</a>
-    </nav>
     <div id='content'>
         <p>{$text}</p>
         <p>{$result}</p>
+        <a href='index.html' class='button'>Volver al Menú</a>
     </div>
 </body>
 </html>";
 ?>
+
