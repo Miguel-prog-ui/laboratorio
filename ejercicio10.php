6a1ddb27960f1088bbf24a42d8065517ea153a42
Weekend2101 <liviogar20@gmail.com>
2024-09-23 19:40:12 -0400

    Add files via upload

---
+++ ejercicio10.php
@@ -0,0 +1,32 @@
+<html> 
+ 
+<head> 
+  <title>Ejemplo de operaciones</title> 
+</head> 
+ 
+<body> 
+<h1>Ejemplo de operaciones aritmeticas en PHP</h1> 
+<?php 
+ 
+$a = 8; 
+$b = 3; 
+echo "Suma", "<br>";
+echo "8 + 3 " , " = ",  $a + $b,  "<br>"; 
+echo "Resta", "<br>";
+echo "8 - 3 " , " = ", $a - $b, "<br>";
+echo "Multiplicacion", "<br>";
+echo "8 * 3 " , " = ", $a * $b, "<br>"; 
+echo "Division", "<br>";
+echo "8 / 3 " , " = ", $a / $b, "<br>"; 
+ 
+$a++ ; 
+echo $a,"<br>"; 
+$b--; 
+echo $b,"<br>"; 
+ 
+?> 
+ 
+</body> 
+ 
+</html> 
+ 
