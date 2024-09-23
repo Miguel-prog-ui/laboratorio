d29242cb41bd7193222e9e941b6fc3d3d2f78f91
Miguel-prog-ui <mmijares2033@gmail.com>
2024-09-23 17:54:01 -0400

    ola

--- ejercicio30.php
+++ ejercicio30.php
@@ -1,9 +1,11 @@
-<html> 
-<head> 
-<title>Un libro de visitas muy sencillo</title> 
-<meta http-equiv="content-type" content="text/html; charset=iso-8859-1"> 
-</head> 
-<body> 
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
 <h1>Libro de visitas</h1> 
 <form action="<?php echo $_SERVER['PHP_SELF']; ?>" method="post" 
 Tu comentario:<br> 
@@ -14,39 +16,41 @@ Tu e-mail:<br>
 <input type="text" name="email"><br> 
 <input type="submit" value="publica:"> 
 </form> 
-<h3>Mostrar todos los comentarios</h3> 
+<h3>Mostrar todos los comentarios</h3>
+</body>
+</html>
 <?php 
 //Guarda el nombre del archivo en la variable 
 $file ="guestbook.txt"; 
 //¿variable comment definida? ¿Nombre e e-mail no estan vacios? 
 //if isset($_POST['comment']) && $_POST['name'] ! = "" && $_POST['email'] != "" { 
- if (isset($_POST['comment']) && isset($_POST['name']) != "" && $_POST['email'] != "") { 
- $comment = $_POST['comment']; 
- $name = $_POST['name']; 
- $email = $_POST['email']; 
- //El archivo se abre para escritura-lectura 
- $fp = fopen($file, "r+"); 
- //Leer todos los datos y almacenar en $old
- $old = fread ($fp, filesize($file)); 
- //Se crea el vinculo de e-mail 
- $email = "<a href=\mailto:$email\">$email</a>"; 
- //Se incluye la fecha y se le da formato 
- $dateOfEntry = date ("y-n-j"); 
- //Ocultar caracteres html, eliminar slashes, mantener saltos de linea 
- $comment = htmlspecialchars($comment); 
-// $comment = stripslashes(n12br($comment)); 
- $comment = stripslashes($comment); 
- //"Montar la entrada (entry) del libro de visitas 
- $entry="<p><b>$name</b> ($email) wrote on <i>$dateOfentry</i>;<br>$comment</p>\n"; 
- //El cursor invisible salta al principio 
- rewind($fp); 
- //Escribir en la nueva entrada antes de las antiguas en el archivo: 
- fputs($fp, "$entry \n $old"); 
- //cerrar rl archivo 
- fclose($fp); 
+    if (isset($_POST['comment']) && isset($_POST['name']) != "" && $_POST['email'] != "") { 
+        $comment = $_POST['comment']; 
+        $name = $_POST['name']; 
+        $email = $_POST['email']; 
+        //El archivo se abre para escritura-lectura 
+        $fp = fopen($file, "r+"); 
+        //Leer todos los datos y almacenar en $old 
+        $old = fread ($fp, filesize($file)); 
+        //Se crea el vinculo de e-mail 
+        $email = "<a href=\mailto:$email\">$email</a>"; 
+        //Se incluye la fecha y se le da formato 
+        $dateOfEntry = date ("y-n-j"); 
+        //Ocultar caracteres html, eliminar slashes, mantener saltos de linea 
+        $comment = htmlspecialchars($comment); 
+        // $comment = stripslashes(n12br($comment)); 
+            $comment = stripslashes($comment); 
+        //"Montar la entrada (entry) del libro de visitas 
+            $entry="<p><b>$name</b> ($email) wrote on <i>$dateOfEntry</i>;<br>$comment</p>\n"; 
+        //El cursor invisible salta al principio 
+            rewind($fp); 
+        //Escribir en la nueva entrada antes de las antiguas en el archivo: 
+            fputs($fp, "$entry \n $old"); 
+        //cerrar rl archivo 
+            fclose($fp); 
 } 
 //Mostrar el archivo completo 
-readfile($file); 
+    readfile($file); 
 ?> 
 </body> 
 </html>
