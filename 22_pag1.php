3cebc630c340bab25b81886182deb3dc141ff622
Miguel-prog-ui <mmijares2033@gmail.com>
2024-09-24 21:19:05 -0400

    matenme

---
+++ 22_pag1.php
@@ -0,0 +1,16 @@
+<html> 
+<head> 
+ <title>Ejemplo Página 1 Librería</title> 
+</head> 
+<body> 
+<?php include("21_libreria.phtml"); ?> 
+<?php CabeceraPagina(); ?> 
+Página 1 
+<BR><BR><BR><BR><BR> 
+Contenido blalbl blalb alb<BR><BR> 
+más cosas...<BR><BR> 
+fin<BR><BR> 
+<?php PiePagina(); ?> 
+</body> 
+</html>
+
--- ejercicio42.php
+++ ejercicio42.php
@@ -7,11 +7,17 @@
 <h3>Introducir direcciones</h3>
 
 <?php
-include("acceso.inc.php");
+$dp = new mysqli("localhost", "root", "", "agenda");
+
+// Verificar conexión
+if ($dp->connect_error) {
+    die("<p>No se ha podido establecer la conexión con MySQL.</p>");
+}
 
 if (isset($_POST['submit'])) {
+    // Validación de campos
     $errors = [];
-
+    
     if (empty($_POST['Nombre'])) {
         $errors[] = "<p>Introduzca el <b>nombre</b>.</p>";
     }
@@ -19,41 +25,33 @@ if (isset($_POST['submit'])) {
         $errors[] = "<p>El apellido debe tener como mínimo <b>3</b> caracteres.</p>";
     }
 
+    // Mostrar errores
     if (!empty($errors)) {
         foreach ($errors as $error) {
             echo $error;
         }
     } else {
-        $sql = "INSERT INTO direcciones (Tratamiento, Nombre, Apellido, Calle, CP, Localidad, Tel, Movil, Mail, Website, Categoria, Notas) VALUES (?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?)";
-        $stmt = $dp->prepare($sql);
-
-        if ($stmt === false) {
-            die("<p>Error en la preparación de la consulta: " . htmlspecialchars($dp->error) . "</p>");
-        }
-
-        $stmt->bind_param("ssssssssssss", $_POST['Tratamiento'], $_POST['Nombre'], $_POST['Apellido'], $_POST['Calle'], $_POST['CP'], $_POST['Localidad'], $_POST['Tel'], $_POST['Movil'], $_POST['Mail'], $_POST['Website'], $_POST['Categoria'], $_POST['Notas']);
-
+        // Preparar la consulta
+        $stmt = $dp->prepare("INSERT INTO direcciones (Tratamiento, Nombre, Apellido, Calle, CP, Localidad, Tel, Movil, Mail, Website, Categoria, Notas) VALUES (?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?)");
+        $stmt->bind_param("ssssssssssss", $_POST['Tratamiento'], $_POST['Nombre'], $_POST['Apellido'], $_POST['Calle'], $_POST['CP'], $_POST['Localidad'], $_POST['Tel'], $_POST['Movil'], $_POST['Mail'], $_POST['Website'],$_POST['Categoria'] , $_POST['Notas']); 
+        
         if ($stmt->execute()) {
-            echo "<p>Datos agregados con éxito.</p>";
+            echo "<p> Datos agregados con éxito.</p>";
         } else {
-            echo "<p>Datos <b>no</b> agregados: " . htmlspecialchars($stmt->error) . "</p>";
+            echo "<p>Datos <b>no</b> agregados.</p>";
         }
-
+        
         $stmt->close();
         echo "[ <a href='javascript:history.back()'>Volver</a> ] - [ <a href='{$_SERVER['PHP_SELF']}'> Introducir nueva fila</a> ]";
     }
 } else {
+    // Obtener categorías
     $sql2 = "SELECT * FROM categorias";
     $resultado2 = $dp->query($sql2);
-
-    if ($resultado2 === false) {
-        die("<p>Error en la consulta de categorías: " . htmlspecialchars($dp->error) . "</p>");
-    }
-
+    
     $campocat = "";
-
     while ($row = $resultado2->fetch_assoc()) {
-        $campocat .= "<option value='{$row['Categoria']}'>{$row['Categoria']}</option>\n";
+        $campocat .= "<option value='{$row['Categoria']}'>{$row['Categoria']}</option>\n"; 
     }
 }
 ?>
