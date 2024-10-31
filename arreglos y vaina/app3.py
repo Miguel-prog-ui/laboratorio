3d74b2ac0f73282c04675df831a3f1bc9d6aac32
Weekend2101 <liviogar20@gmail.com>
2024-10-30 21:14:50 -0400

    Update app3.py

--- arreglos y vaina/app3.py
+++ arreglos y vaina/app3.py
@@ -1,73 +1,92 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request, redirect, url_for
 
 app = Flask(__name__)
 
+# Crear un arreglo principal que contenga subarreglos para lista, pila, árbol y cola
+estructura_compuesta = []
+
+# Lista
+mi_lista = [1, 2, 3, 4, 5]
+estructura_compuesta.append({"tipo": "Lista", "datos": mi_lista})
+
+# Pila
+pila = []
+pila.append('A')
+pila.append('B')
+pila.append('C')
+estructura_compuesta.append({"tipo": "Pila", "datos": pila})
+
+# Árbol Binario
+class Nodo:
+    def __init__(self, valor):
+        self.valor = valor
+        self.izquierda = None
+        self.derecha = None
+
+def insertar_arbol(raiz, valor):
+    if raiz is None:
+        return Nodo(valor)
+    else:
+        if valor < raiz.valor:
+            raiz.izquierda = insertar_arbol(raiz.izquierda, valor)
+        else:
+            raiz.derecha = insertar_arbol(raiz.derecha, valor)
+    return raiz
+
+def preorden(nodo):
+    resultado = []
+    if nodo:
+        resultado.append(nodo.valor)
+        resultado.extend(preorden(nodo.izquierda))
+        resultado.extend(preorden(nodo.derecha))
+    return resultado
+
+arbol = None
+valores_arbol = [20, 10, 30, 5, 15, 25, 35]
+for valor in valores_arbol:
+    arbol = insertar_arbol(arbol, valor)
+estructura_compuesta.append({"tipo": "Árbol", "datos": preorden(arbol)})
+
+# Cola
+cola = []
+cola.append('Persona 1')
+cola.append('Persona 2')
+cola.append('Persona 3')
+estructura_compuesta.append({"tipo": "Cola", "datos": cola})
+
 @app.route('/')
 def index():
-    # Crear un array que contenga subarrays para lista, pila, cola, lista y árbol
-    estructura_compuesta = []
-
-    # Arreglos
-    frutas = ["Mangos", "Banana", "Uva", "Mora"]
-    estructura_compuesta.append({"tipo": "Arreglos", "datos": frutas})
-
-    # Pilas
-    pila = ["Elemento A", "Elemento B", "Elemento C", "Elemento D", "Elemento E", "Elemento F"]
-    elemento_pila = pila.pop()
-    estructura_compuesta.append({"tipo": "Pilas", "datos": pila, "ultimo": elemento_pila})
-
-    # Colas
-    cola = ["Persona 1", "Persona 2", "Persona 3", "Persona 4", "Persona 5"]
-    cliente_cola = cola.pop(0)
-    estructura_compuesta.append({"tipo": "Colas", "datos": cola, "primero": cliente_cola})
-
-    # Listas
-    lista = ["Lácteos", "Granos", "Carnes"]
-    estructura_compuesta.append({"tipo": "Listas", "datos": lista})
-
-    # Árbol Binario
-    class Nodo:
-        def __init__(self, dato):
-            self.dato = dato
-            self.izquierda = None
-            self.derecha = None
-
-    class ArbolBinario:
-        def __init__(self):
-            self.raiz = None
-
-        def insertar(self, dato):
-            if self.raiz is None:
-                self.raiz = Nodo(dato)
-            else:
-                self._insertar(dato, self.raiz)
-
-        def _insertar(self, dato, nodo_actual):
-            if dato < nodo_actual.dato:
-                if nodo_actual.izquierda is None:
-                    nodo_actual.izquierda = Nodo(dato)
-                else:
-                    self._insertar(dato, nodo_actual.izquierda)
-            else:
-                if nodo_actual.derecha is None:
-                    nodo_actual.derecha = Nodo(dato)
-                else:
-                    self._insertar(dato, nodo_actual.derecha)
-
-        def imprimir(self, nodo, resultado):
-            if nodo is not None:
-                self.imprimir(nodo.izquierda, resultado)
-                resultado.append(nodo.dato)
-                self.imprimir(nodo.derecha, resultado)
-
-    arbol = ArbolBinario()
-    for valor in [20, 15, 25, 10, 18, 22, 30]:
-        arbol.insertar(valor)
-    resultado_arbol = []
-    arbol.imprimir(arbol.raiz, resultado_arbol)
-    estructura_compuesta.append({"tipo": "Árboles", "datos": resultado_arbol})
-
     return render_template('index.html', estructura_compuesta=estructura_compuesta)
+
+@app.route('/agregar_lista', methods=['POST'])
+def agregar_lista():
+    item = request.form['item']
+    estructura_compuesta[0]["datos"].append(item)
+    return redirect(url_for('index'))
+
+@app.route('/agregar_pila', methods=['POST'])
+def agregar_pila():
+    item = request.form['item']
+    estructura_compuesta[1]["datos"].append(item)
+    return redirect(url_for('index'))
+
+@app.route('/eliminar_pila', methods=['POST'])
+def eliminar_pila():
+    if estructura_compuesta[1]["datos"]:
+        estructura_compuesta[1]["datos"].pop()
+    return redirect(url_for('index'))
+
+@app.route('/agregar_cola', methods=['POST'])
+def agregar_cola():
+    item = request.form['item']
+    estructura_compuesta[3]["datos"].append(item)
+    return redirect(url_for('index'))
+
+@app.route('/eliminar_cola', methods=['POST'])
+def eliminar_cola():
+    if estructura_compuesta[3]["datos"]:
+        estructura_compuesta[3]["datos"].pop(0)
+    return redirect(url_for('index'))
 
 if __name__ == "__main__":
     app.run(debug=True, port=5000)
